16b16b79928d290592ccedea10d93656b224ddd5
Jeberwein <104577021+Jeberwein@users.noreply.github.com>
2024-10-02 19:25:04 +0200

    seas

--- main.py
+++ main.py
@@ -1,24 +1,37 @@
 import random
 import matplotlib.pyplot as plt
+
 statsNumbs = {i: 0 for i in range(1, 46)}
+
+
 def lotto():
-    for g in range(1000):
-        PulledNumbs = []
-        for i in range(6):
-            AllNumbs = [i for i in range(1, 46)]
-            randomNumb = random.randint(0, 44 - i)
-            PulledNumb = AllNumbs[randomNumb]
-            lastNumb = AllNumbs[44 - i]
-            AllNumbs[44 - i] = PulledNumb
-            AllNumbs[randomNumb] = lastNumb
-            PulledNumbs.append(PulledNumb)
-        Stats(PulledNumbs)
-def Stats(PulledNumbs):
-    for i in PulledNumbs:
+    pulledNumbs = []
+    allNumbs = [i for i in range(1, 46)]
+    for i in range(6):
+        randomNumb = random.randint(0, 44 - i)
+        pulledNumb = allNumbs[randomNumb]
+        lastNumb = allNumbs[44 - i]
+        allNumbs[44 - i] = pulledNumb
+        allNumbs[randomNumb] = lastNumb
+        pulledNumbs.append(pulledNumb)
+
+    print(f'Gezogene Lottozahlen: {pulledNumbs}')
+    return pulledNumbs
+
+
+def ziehungen(anzahl=1000):
+    for _ in range(anzahl):
+        pulledNumbs = lotto()
+        Stats(pulledNumbs)
+
+
+def Stats(pulledNumbs):
+    for i in pulledNumbs:
         statsNumbs[i] += 1
 
+
 if __name__ == "__main__":
-    lotto()
+    ziehungen()
     keys = list(statsNumbs.keys())
     values = list(statsNumbs.values())
     plt.bar(keys, values)
